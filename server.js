5f201108db195363d2f6208ffd0473bc402e762a
Allen <a1b2c3d42003hongkong@hotmail.com>
2016-07-21 18:18:28 +0800

    Add TLS server secureOptions and its configuration Fixed Typo Move some code so that run on child only

--- server.js
+++ server.js
@@ -1,82 +1,5 @@
-var fs      = require('fs'),
-    tls     = require('tls'),
-    net     = require('net'),
-    crypto  = require('crypto'),
-    cluster = require('cluster'),
+var cluster = require('cluster'),
     conf    = require('./conf.json');
-
-var noOp = function(){};
-var certCache = {};
-var getCert = function(domain){
-	var key, crt, ca;
-	if( !conf.site[domain] )	throw new Error('Domain Not Set Exception: Domain: ' + domain);
-
-	if(conf.site[domain].keyPath){
-		try{
-			key = fs.readFileSync(conf.site[domain].keyPath);
-		}catch(e){}
-	}
-	if(conf.site[domain].crtPath){
-		try{
-			crt = fs.readFileSync(conf.site[domain].crtPath);
-		}catch(e){}
-	}
-	if(conf.site[domain].caPath){
-		try{
-			ca = fs.readFileSync(conf.site[domain].caPath);
-		}catch(e){}
-	}
-	if(conf.certPath){
-		if(!key){
-			try{
-				key = fs.readFileSync(conf.certPath + '/' + domain + '/.key');
-			}catch(e){}
-		}
-		if(!crt){
-			try{
-				crt = fs.readFileSync(conf.certPath + '/' + domain + '/.crt');
-			}catch(e){}
-		}
-		if(!ca){
-			try{
-				ca = fa.readFileSync(conf.certPath + '/' + domain + '/.ca');
-			}catch(e){}
-		}
-	}
-	if(!key || !crt){
-		if(domain != conf.defaultDomain){
-			console.log(
-				"Cert Not Found: \n"
-				+ "\tDomain: " + domain + "\n"
-				+ "\tPath:\n"
-				+ "\t\t" + (!!conf.site[domain].keyPath ? conf.site[domain].keyPath + ' or ' : '' ) + conf.certPath + '/' + domain + '/.key'
-				+ "\t\t" + (!!conf.site[domain].crtPath ? conf.site[domain].crtPath + ' or ' : '' ) + conf.certPath + '/' + domain + '/.crt'
-			);
-			return getCert(conf.defaultDomain);
-		}else{
-			throw new Error(
-				'Default Cert Not Found Exception: ' + "\n"
-				+ (!key ? (!!conf.site[domain].keyPath ? conf.site[domain].keyPath + ' or ' : '' ) + conf.certPath + '/' + domain + '/.key' + "\n" : '')
-				+ (!crt ? (!!conf.site[domain].crtPath ? conf.site[domain].crtPath + ' or ' : '' ) + conf.certPath + '/' + domain + '/.crt' + "\n" : '')
-			);
-		}
-	}
-	return {
-		key : key,
-		cert : crt,
-		ca : ca
-	};
-}
-var getCertContext = function(domain){
-	if(!certCache[domain]) certCache[domain] = crypto.createCredentials(getCert(domain)).context;
-	return certCache[domain];
-}
-
-var httpsServerOptions = (function(){
-	var options = getCert(conf.defaultDomain);
-	options.SNICallback = getCertContext;
-	return options;
-})();
 
 if(cluster.isMaster){
 	var MaxTry = conf.maxTryStart || 100;
@@ -96,6 +19,94 @@ if(cluster.isMaster){
 		}
 	});
 }else{
+	var fs      = require('fs'),
+	    tls     = require('tls'),
+	    net     = require('net'),
+	    crypto  = require('crypto');
+
+	var noOp = function(){};
+	var certCache = {};
+	var getCert = function(domain){
+		var key, crt, ca;
+		if( !conf.site[domain] )	throw new Error('Domain Not Set Exception: Domain: ' + domain);
+
+		if(conf.site[domain].keyPath){
+			try{
+				key = fs.readFileSync(conf.site[domain].keyPath);
+			}catch(e){}
+		}
+		if(conf.site[domain].crtPath){
+			try{
+				crt = fs.readFileSync(conf.site[domain].crtPath);
+			}catch(e){}
+		}
+		if(conf.site[domain].caPath){
+			try{
+				ca = fs.readFileSync(conf.site[domain].caPath);
+			}catch(e){}
+		}
+		if(conf.certPath){
+			if(!key){
+				try{
+					key = fs.readFileSync(conf.certPath + '/' + domain + '/.key');
+				}catch(e){}
+			}
+			if(!crt){
+				try{
+					crt = fs.readFileSync(conf.certPath + '/' + domain + '/.crt');
+				}catch(e){}
+			}
+			if(!ca){
+				try{
+					ca = fs.readFileSync(conf.certPath + '/' + domain + '/.ca');
+				}catch(e){}
+			}
+		}
+		if(!key || !crt){
+			if(domain != conf.defaultDomain){
+				console.log(
+					"Cert Not Found: \n"
+					+ "\tDomain: " + domain + "\n"
+					+ "\tPath:\n"
+					+ "\t\t" + (!!conf.site[domain].keyPath ? conf.site[domain].keyPath + ' or ' : '' ) + conf.certPath + '/' + domain + '/.key'
+					+ "\t\t" + (!!conf.site[domain].crtPath ? conf.site[domain].crtPath + ' or ' : '' ) + conf.certPath + '/' + domain + '/.crt'
+				);
+				return getCert(conf.defaultDomain);
+			}else{
+				throw new Error(
+					'Default Cert Not Found Exception: ' + "\n"
+					+ (!key ? (!!conf.site[domain].keyPath ? conf.site[domain].keyPath + ' or ' : '' ) + conf.certPath + '/' + domain + '/.key' + "\n" : '')
+					+ (!crt ? (!!conf.site[domain].crtPath ? conf.site[domain].crtPath + ' or ' : '' ) + conf.certPath + '/' + domain + '/.crt' + "\n" : '')
+				);
+			}
+		}
+		return {
+			key : key,
+			cert : crt,
+			ca : ca
+		};
+	}
+	var getCertContext = function(domain){
+		if(!certCache[domain]) certCache[domain] = crypto.createCredentials(getCert(domain)).context;
+		return certCache[domain];
+	}
+
+	var httpsServerOptions = (function(){
+		var options = getCert(conf.defaultDomain);
+		options.SNICallback = getCertContext;
+		options.secureProtocol = 'SSLv23_method';
+
+		var constants = require('constants');
+
+		options.secureOptions = 0;
+		for(var i=0; i<conf.sslSecureOptions.length; i++){
+			if(!!conf.sslSecureOptions[i].match(/^SSL_OP_/)){
+				options.secureOptions |= constants[conf.sslSecureOptions[i]];
+			}
+		}
+		return options;
+	})();
+
 	tls.createServer(httpsServerOptions, function(socket){
 		if(conf.site[socket.servername]){
 			var remote = (conf.site[socket.servername].https ? tls : net).connect(
